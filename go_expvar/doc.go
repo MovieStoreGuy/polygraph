af2af8fba236198917e80edf3ec41d017703dae5
Sean ZO Marciniak <sean@beamery.com>
2018-12-10 14:44:52 +0000

    Fixing doc format

--- go_expvar/doc.go
+++ go_expvar/doc.go
@@ -1,8 +1,8 @@
 // Package go_expvar allows for metrics exporting using the expvar package
 // This to take note of are:
-// 	- The labels applied to struct variables must be unique otherwise you
+//  - The labels applied to struct variables must be unique otherwise you
 //    risk over writing a previously set variable
-//  	- Stick to a naming convention like `polygraph:"package.class.(innerStructName)*.variable"`
+//    - Stick to a naming convention like `polygraph:"package.class.(innerStructName)*.variable"`
 //  - Any value that is nil once passed through the Publish function will be ignored
 //  - Once something is added to Publish method, it can not be removed.
 package go_expvar
